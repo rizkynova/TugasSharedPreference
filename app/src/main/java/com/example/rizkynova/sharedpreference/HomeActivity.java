bccafa13246cf3dc8b351e1355948cacb0e8e461
rizkynova <rizkynovayandiofficial@gmail.com>
2020-04-30 19:42:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/rizkynova/sharedpreference/HomeActivity.java
@@ -0,0 +1,54 @@
+package com.example.rizkynova.sharedpreference;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rizkynova.R;
+import com.example.rizkynova.sharedpreferences.utils.Preferences;
+
+/*
+
+Nama  : Rizky Novayandi
+Kelas : IF4
+NIM   : 10117140
+Email : rizkynovayandiofficial@gmail.com
+
+ */
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView txtKeluar;
+    private TextView txtName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        declareView();
+        txtKeluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Clear Set Preferences
+                Preferences.setLogout(getBaseContext());
+
+                //Pindah Halaman ke Login
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+    private void declareView() {
+        txtKeluar = findViewById(R.id.txt_logout);
+        txtName = findViewById(R.id.txtName);
+
+        txtName.setText(Preferences.getRegisteredUser(getBaseContext()));
+    }
+}
